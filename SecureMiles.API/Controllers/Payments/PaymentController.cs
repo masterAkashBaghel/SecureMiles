64ba17d7e78c02f1f3af5279e4189174a0e17c22
masterAkashBaghel <officeofakash@gmail.com>
2024-11-27 23:43:14 +0530

    docker files and minor bug fix

---
+++ SecureMiles.API/Controllers/Payments/PaymentController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureMiles.API.Controllers.Payments
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+
+    }
+}
